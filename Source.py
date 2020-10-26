049e26fa3b8abbd17313588ab278e64f45477886
Matheus Dias Vieira <matheuspvh13@gmail.com>
2020-10-25 22:52:54 -0400

    Docs Improvement on getVideoIdsFromRemoteTxt()

--- Source.py
+++ Source.py
@@ -5,7 +5,15 @@ class Source(object):
     @staticmethod
     def getVideoIdsFromRemoteTxt(country: str, date: str) -> list:
         """
-        Static method to get data from remote Txt file
+        Static method to consult the remote TXT file (hosted on GitHub) and return a list with video's 
+        ID from trending on determinate day.
+
+        Parameters:
+            country (str): the country from the video's trending
+            date (str): specific day from the trend video list
+
+        Return:
+            videoIdsList (list[str]): list with the list of video's ID on the trending day
         """
         BASE_URL = "https://raw.githubusercontent.com/ophobe/trending-youtube/master/"
         videoIdsList = []
